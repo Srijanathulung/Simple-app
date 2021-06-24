0f789ed6dda6a93f218b0d6b19df18db0f289498
Srijana Thulung <srijana.thulung123@gmail.com>
2021-06-24 10:11:41 +0545

    replacing setState with ref

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
@@ -6,24 +6,35 @@ import ErrorModal from '../UI/ErrorModal';
 import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
+    //connecting ref with input
+    const nameInputRef = useRef();//its an object and always hold the current value
+    const ageInputRef = useRef();
 
-    const [enteredUserName, setEnteredUserName] = useState('')
-    const [enteredUserAge, setEnteredUserAge] = useState('')
+    // we are using 'ref' to get the value so all setState can be removed from the code
+    // const [enteredUserName, setEnteredUserName] = useState('')
+    // const [enteredUserAge, setEnteredUserAge] = useState('')
+
     const [error, setError] = useState();
     
     
    
     //It listen to the event
-    const userNameChangeHandler = (event) => {
-         setEnteredUserName(event.target.value)
-    }
+    // const userNameChangeHandler = (event) => {
+    //      setEnteredUserName(event.target.value)
+    // }
 
-    const userAgeChangeHandler = (event) => {
-        setEnteredUserAge(event.target.value)
-    }
+    // const userAgeChangeHandler = (event) => {
+    //     setEnteredUserAge(event.target.value)
+    // }
+
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (enteredUserName.trim().length === 0 || enteredUserAge.trim().length=== 0) {
+        // console.log(nameInputRef);
+        // console.log(nameInputRef.current.value);
+        const enteredName = nameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
+        // if (enteredUserName.trim().length === 0 || enteredUserAge.trim().length=== 0) {
+        if (enteredName.trim().length === 0 || enteredAge.trim().length=== 0) {
             // console.log('Enter the username and id')
             setError({
                 title: 'Invalid input',
@@ -32,17 +43,22 @@ const AddUser = (props) => {
             return; 
         } 
         //+enteredUserAge '+' works as a parseInt
-        if (+enteredUserAge < 1) {
+        // if (+enteredUserAge < 1) {
+        if (+enteredAge < 1) {
             setError({
                 title: 'invalid age',
                 message: 'Please enter a valid age greater than 0'
             })
             return;
         }
-        props.onAddUser(enteredUserName,enteredUserAge);
-            console.log('enteredUserName=' + enteredUserName + ' enteredUserAge=' + enteredUserAge)
-            setEnteredUserName('');
-            setEnteredUserAge('');
+        // props.onAddUser(enteredUserName,enteredUserAge);
+        props.onAddUser(enteredName,enteredAge);
+        console.log('enteredName=' + enteredName + ' enteredAge=' + enteredAge)
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
+            // console.log('enteredUserName=' + enteredUserName + ' enteredUserAge=' + enteredUserAge)
+            // setEnteredUserName('');
+            // setEnteredUserAge('');
         // console.log(enteredUserName, enteredUserAge)
     };
     
@@ -70,16 +86,19 @@ const AddUser = (props) => {
                 <input
                     type='text'
                     id='username'
-                    value={enteredUserName}
-                    onChange={(event) => userNameChangeHandler(event)}
+                    // value={enteredUserName}
+                    // onChange={(event) => userNameChangeHandler(event)}
+                    ref={nameInputRef}    
                 />
                 
             <label htmlFor='age'>Age(years)</label>
                 <input
                     type='number'
                     id='age'
-                    value={enteredUserAge}
-                    onChange={(event) => userAgeChangeHandler(event) }
+                    // value={enteredUserAge}
+                    // onChange={(event) => userAgeChangeHandler(event)}
+                    ref={ageInputRef}    
+    
                 />
                 
                     <Button
